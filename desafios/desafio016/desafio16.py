6ada682d013c7eab9c86bddffbd983dd69871897
jonathanmenezesm <jonathanmenezesm@gmail.com>
2026-02-03 10:44:46 -0300

    Desafio16 adicionado

---
+++ desafios/desafio016/desafio16.py
@@ -0,0 +1,3 @@
+# Crie uma classe Funcionario, onde podemos cadastrar: nome, setor e cargo. Crie também um método que permita ao funcionário se apresentar.
+
+#Apresentar > "Olá, eu sou {nome} e sou {cargo} do setor de {setor}.
