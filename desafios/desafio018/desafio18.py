b669be8f6ebc025fd6404aa2e80aa045e76ddd9e
jonathanmenezesm <jonathanmenezesm@gmail.com>
2026-02-03 10:45:30 -0300

    Desafio18 adicionado

---
+++ desafios/desafio018/desafio18.py
@@ -0,0 +1,11 @@
+# Crie uma classe Churrasco(titulo:, quantidade:), onde seja possível informar quantas pessoas vão participar e mostre quanto de carne deve ser comprado, o custo total do churrasco e o preço por pessoa.
+
+# saída:
+# considere 400g por pessoa
+# preço da carne: R$82,40/kg
+# analisar():
+# quadrado usando rich
+# Analisando "titulo" com "convidados"
+# cada participante comerá 0.4kg e cada Kg custa R$82.40
+#O custo total será de "custo"
+# Cada pessoa pagará "custo individual"
