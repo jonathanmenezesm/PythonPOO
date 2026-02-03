292375869a247ee8a422d5c3047753e76a10289a
jonathanmenezesm <jonathanmenezesm@gmail.com>
2026-02-03 10:55:57 -0300

    Desafio22 adicionado

---
+++ desafios/desafio022/desafio22.py
@@ -0,0 +1,9 @@
+# 📺 Crie a classe ControleRemoto, onde vamos simular o funcionamento
+# 📺 de um controle simples (canal, volume e liga/desliga)
+
+# < Diminui o canal
+# > aumenta o canal
+# + Aumenta volume
+# - Diminui o volume
+# @ liga/desliga a tv
+# 0 sai do programa
