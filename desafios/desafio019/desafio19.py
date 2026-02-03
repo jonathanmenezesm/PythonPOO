e400e3f82ebe3d18679707d585bbce859dc3d6d6
jonathanmenezesm <jonathanmenezesm@gmail.com>
2026-02-03 10:45:44 -0300

    Desafio19 adicionado

---
+++ desafios/desafio019/desafio19.py
@@ -0,0 +1,10 @@
+# Crie a classe Livro, que vai simular a passagem de páginas de um livro,
+# considerando também se o usuário chegou ao fim da leitura.
+
+#Classe > Titulo e paginas
+
+#terminal
+# 📖 Você acabou de abrir o livro '10 coisas que aprendi' que tem 20 páginas no total.
+# 📖 Você agora está na página 1
+# 📖 ► Pág2 ► Pág3 ► Pág4 ► Pág5 ► Pág6 ► Você avançou 5 páginas e agora está na página 6
+# 📖 ► Pág7 ► Pág8 ► Pág9 ► Pág10 ► Pág11 ► Pág12 ► Pág13 ► Pág14 ► Pág15 ► Pág16 ► Você avançou 10 páginas e agora está na página 16
