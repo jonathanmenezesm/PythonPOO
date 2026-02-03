abf70ed230e0fd6e18744ca64769e6ae5256b841
jonathanmenezesm <jonathanmenezesm@gmail.com>
2026-02-03 10:52:30 -0300

    Desafio21 adicionado

---
+++ desafios/desafio021/desafio21.py
@@ -0,0 +1,3 @@
+# 🖊️ Crie a classe Caneta, que simule o funcionamento de uma caneta colorida,
+# 🖊️ podendo escrever frases na cor relativa.
+# OBS.: Crie o metodo destampar(), caso esteja tampada a caneta não escreve e tambem o metodo tampar()
