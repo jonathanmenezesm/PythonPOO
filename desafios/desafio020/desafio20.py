a5549941aea5f60f5741701fe81f49476ccacd2c
jonathanmenezesm <jonathanmenezesm@gmail.com>
2026-02-03 10:49:12 -0300

    Desafio19 adicionado

---
+++ desafios/desafio020/desafio20.py
@@ -0,0 +1,17 @@
+# 🎮 Crie a classe Gamer, onde podemos cadastrar nome, nick e os jogos favoritos de uma pessoa.
+# 🎮 Crie também um método que permita mostrar a ficha desse gamer.
+
+# j1 = Gamer(nome="Fabricio da Silva", nick="detonator2025")
+# j1.add_favoritos("Mario Bros.")
+# j1.add_favoritos("Sonic")
+# j1.add_favoritos("God of War")
+# j1.add_favoritos("Fortnite")
+# j1.ficha()
+#
+# Jogador <detonator2025>
+# Nome real: Fabricio da Silva
+# Jogos favoritos:
+#   Fortnite
+#   God of War
+#   Mario Bros.
+#   Sonic
