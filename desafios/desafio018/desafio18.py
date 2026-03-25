4f8676647b7dc968a69bb0b275a10a2ccbe597d2
jonathanmenezesm <jonathanmenezesm@gmail.com>
2026-03-25 01:11:54 -0300

    Desafio 18 - Iniciado

--- desafios/desafio018/desafio18.py
+++ desafios/desafio018/desafio18.py
@@ -4,8 +4,20 @@
 # considere 400g por pessoa
 # preço da carne: R$82,40/kg
 # analisar():
+
+
 # quadrado usando rich
 # Analisando "titulo" com "convidados"
 # cada participante comerá 0.4kg e cada Kg custa R$82.40
-#O custo total será de "custo"
-# Cada pessoa pagará "custo individual"
+# Recomendo comprar "quantidade_carne" kg de carne
+# O custo total será de "custo"
+# Cada pessoa pagará "custo individual" para participar do churrasco
+
+from rich.panel import Panel
+from rich.console import Console
+
+class Churrasco:
+    def __init__(self, titulo, quantidade):
+        self.titulo = titulo
+        self.quantidade = quantidade
+     
