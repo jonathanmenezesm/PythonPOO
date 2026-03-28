2e82fb5d43d743565a53b3bc50c632527bcf45b9
jonathanmenezesm <jonathanmenezesm@gmail.com>
2026-03-28 00:44:02 -0300

    Desafio 18 - completo

--- desafios/desafio018/desafio18.py
+++ desafios/desafio018/desafio18.py
@@ -20,4 +20,23 @@ class Churrasco:
     def __init__(self, titulo, quantidade):
         self.titulo = titulo
         self.quantidade = quantidade
+        
+        
+    def analisar(self):
+        carne_por_pessoa = 0.4
+        preco_carne = 82.40
+        
+        quantidade_carne = self.quantidade * carne_por_pessoa
+        custo_total = quantidade_carne * preco_carne
+        custo_individual = custo_total / self.quantidade
+        
+        console = Console()
+        painel = Panel(f"Analisando '{self.titulo}' com {self.quantidade} convidados\n"
+                       f"Cada participante comerá {carne_por_pessoa} kg e cada Kg custa R$ {preco_carne:.2f}\n"
+                       f"Recomendo comprar {quantidade_carne:.2f} kg de carne\n"
+                       f"O custo total será de R$ {custo_total:.2f}\n"
+                       f"Cada pessoa pagará R$ {custo_individual:.2f} para participar do churrasco", title="Análise do Churrasco", width=70)
+        console.print(painel)
+churrasco = Churrasco("Churrasco dos amigos", 10)
+churrasco.analisar()
      
