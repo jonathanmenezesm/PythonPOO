24ba319c0540b0aef1d09910827b220d8a4fcc77
jonathanmenezesm <jonathanmenezesm@gmail.com>
2026-07-23 11:02:11 -0300

    Desafio18 concluído

--- desafios/desafio018/desafio18.py
+++ desafios/desafio018/desafio18.py
@@ -1,5 +1,6 @@
-# Crie uma classe Churrasco(titulo:, quantidade:), onde seja possível informar quantas pessoas vão participar e mostre quanto de carne deve ser comprado, o custo total do churrasco e o preço por pessoa.
-
+# Crie uma classe Churrasco(titulo:, quantidade:), onde seja possível informar quantas pessoas vão participar
+# mostre quanto de carne deve ser comprado,
+# o custo total do churrasco e o preço por pessoa.
 # saída:
 # considere 400g por pessoa
 # preço da carne: R$82,40/kg
@@ -9,3 +10,36 @@
 # cada participante comerá 0.4kg e cada Kg custa R$82.40
 #O custo total será de "custo"
 # Cada pessoa pagará "custo individual"
+
+from rich.panel import Panel
+from rich import print
+
+
+class Churrasquinho():
+    def __init__(self,titulo,quantidadeDePessoas): #construtor
+        self.titulo = titulo
+        self.quantidadeDePessoas = quantidadeDePessoas
+
+    def analisar(self):
+        precoDaCarne = 82.40  # preço da carne/kg
+        consumoPadrao = 400 / 1000  # consumo padrão de cada pessoa (dividido por 1000 para converter em gramas)
+
+
+        quantidadeDeCarneNecessaria = consumoPadrao * self.quantidadeDePessoas
+        valorTotalDasCarnes = quantidadeDeCarneNecessaria * precoDaCarne
+        valorPorPessoa = (valorTotalDasCarnes / self.quantidadeDePessoas)
+
+        texto = (f"""
+        Analisando [green]{self.titulo}[/] evento para [blue]{self.quantidadeDePessoas} pessoas[/]
+        Cada pessoa irá consumir 400gr e cada Kg custa R$82,40
+        Recomendo [blue]comprar {round(quantidadeDeCarneNecessaria)}kgs[/] de carne
+        O custo total será de [green]R${valorTotalDasCarnes:.2f}[/]
+        E cada pessoa pagará [yellow]R${valorPorPessoa:.2f}[/] para participar        
+        """)
+
+        caixa = Panel(texto, title=self.titulo)
+
+        print(caixa)
+
+c1 = Churrasquinho("Churrascria", 50)
+c1.analisar()
