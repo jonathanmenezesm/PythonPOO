793b4114facdd76468bc71ed5445a30e83fe04e5
jonathanmenezesm <jonathanmenezesm@gmail.com>
2026-07-24 02:31:23 -0300

    Desafio20 concluído

--- desafios/desafio020/desafio20.py
+++ desafios/desafio020/desafio20.py
@@ -1,6 +1,6 @@
 # 🎮 Crie a classe Gamer, onde podemos cadastrar nome, nick e os jogos favoritos de uma pessoa.
 # 🎮 Crie também um método que permita mostrar a ficha desse gamer.
-
+from openpyxl.styles.builtins import title
 # j1 = Gamer(nome="Fabricio da Silva", nick="detonator2025")
 # j1.add_favoritos("Mario Bros.")
 # j1.add_favoritos("Sonic")
@@ -15,3 +15,37 @@
 #   God of War
 #   Mario Bros.
 #   Sonic
+
+from rich import print
+from rich.panel import Panel
+
+
+
+
+class Gamer():
+    def __init__(self, nome, nick, jogos_favoritos=""):
+        self.nome = nome
+        self.nick = nick
+        self.jogos_favoritos = []
+
+    def add_favoritos(self, jogo):
+        self.jogos_favoritos.append(jogo)
+
+
+    def ficha(self):
+        texto = f" Nome real: [black on blue] {self.nome} [/]\n"
+        texto += "Jogos favoritos:\n"
+        for jogo in sorted(self.jogos_favoritos):
+            texto += f"🎮 [blue]{jogo}[/]\n"   # ícone + cor
+
+        caixa = Panel(texto, title=f"Jogador <{self.nick}>", width=50)
+        print(caixa)
+
+
+j1 = Gamer("Jonathan Moura", "JotaM")
+j1.add_favoritos("Mario")
+j1.add_favoritos("God of War")
+j1.add_favoritos("Zelda")
+j1.ficha()
+
+
